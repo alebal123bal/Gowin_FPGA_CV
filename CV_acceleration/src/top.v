b4b40c199610f911be561656ff365178f9d093ee
alebal123bal <balzanalessandro2001@gmail.com>
2024-12-27 17:22:45 +0100

    Correctly check freq 5x

--- CV_acceleration/src/top.v
+++ CV_acceleration/src/top.v
@@ -3,18 +3,44 @@
 module top (
     input clk,          // 27 MHz clock
     input rst_n,        // Active low reset
-    input sel_button,   // Input for source selection
     output uart_tx,     // UART TX pin
-    output Voltage,
-    output PLL_ok
+    output Voltage
 );
 
 // Parameters
 parameter CLK_FREQ = 27;        // 27 MHz
 parameter BAUD_RATE = 115200;   // 115200 bps
 
+// Signals
+wire clk_135_MHz;
+
+// Registers
+reg[31:0] counter_135_MHz;
+reg Voltage_reg;
+
+// Instantiate rPLL at 135MHz
+Gowin_rPLL PLL_135_MHz(
+    .clkout(clk_135_MHz), //output clkout
+    .clkin(clk) //input clkin
+);
+
+// Quickly check if LED blinks every 1 second at 135MHz 
+always @(posedge clk_135_MHz or negedge rst_n) begin
+    if (!rst_n) begin
+        counter_135_MHz <= 0;
+        Voltage_reg <= 1'b0;
+    end else begin
+        if (counter_135_MHz == 135_000_000 - 1) begin
+            counter_135_MHz <= 0;
+            Voltage_reg <= ~Voltage_reg;
+        end else begin
+            counter_135_MHz <= counter_135_MHz + 1;
+            Voltage_reg <= Voltage_reg;
+        end
+    end
+end
 
 // Combinatorial logic
-assign Voltage = !rst_n;
+assign Voltage = Voltage_reg;
 
 endmodule
