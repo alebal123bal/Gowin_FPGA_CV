008582f3ffe0490a5466d2da0f24d6b5ba49a22b
alebal123bal <balzanalessandro2001@gmail.com>
2025-09-08 21:34:39 +0200

    Add FIFO HS IP

---
+++ CV_acceleration/src/fifo_hs/fifo_hs_tmp.v
@@ -0,0 +1,27 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.12 (64-bit)
+//Part Number: GW2A-LV18PG256C8/I7
+//Device: GW2A-18
+//Device Version: C
+//Created Time: Mon Sep  8 21:32:05 2025
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+	FIFO_HS_Top your_instance_name(
+		.Data(Data), //input [15:0] Data
+		.WrClk(WrClk), //input WrClk
+		.RdClk(RdClk), //input RdClk
+		.WrEn(WrEn), //input WrEn
+		.RdEn(RdEn), //input RdEn
+		.Rnum(Rnum), //output [16:0] Rnum
+		.Almost_Empty(Almost_Empty), //output Almost_Empty
+		.Almost_Full(Almost_Full), //output Almost_Full
+		.Q(Q), //output [7:0] Q
+		.Empty(Empty), //output Empty
+		.Full(Full) //output Full
+	);
+
+//--------Copy end-------------------
